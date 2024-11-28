785edd580a7f6951f903466784d7c47791fc5bb3
nikhilbabasahebshinde <nikhilbabasahebshinde@gmail.com>
2024-11-28 14:38:30 +0530

    First Commit

---
+++ src/main/java/com/management/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.management.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.management.Model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
